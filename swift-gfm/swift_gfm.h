70fc8a32409c25e94d53e954696d2db90115a3ed
lachlanbell <lbell@lachlans-macbook-pro.local>
2017-09-30 09:06:05 +0800

    Initial Commit

---
+++ swift-gfm/swift_gfm.h
@@ -0,0 +1,20 @@
+//
+//  swift_gfm.h
+//  swift-gfm
+//
+//  Created by Lachlan  Bell on 30/9/17.
+//  Copyright © 2017 Lachlan Bell. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+#import "cmark.h"
+
+//! Project version number for swift_gfm.
+FOUNDATION_EXPORT double swift_gfmVersionNumber;
+
+//! Project version string for swift_gfm.
+FOUNDATION_EXPORT const unsigned char swift_gfmVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <swift_gfm/PublicHeader.h>
+
+
